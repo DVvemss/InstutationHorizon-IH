c9a1fc490d6664946b211029a98881cb9d4bfb1e
Diva Fisensa Vemas Sugiharto <115370529+DVvemss@users.noreply.github.com>
2024-05-27 11:04:13 +0700

    Add First Item to the Mod

--- src/main/java/net/dvvemss/instutationhorizon/InstutationHorizon.java
+++ src/main/java/net/dvvemss/instutationhorizon/InstutationHorizon.java
@@ -22,8 +22,6 @@ import org.slf4j.Logger;
 public class InstutationHorizon {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "instutationhorizon";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public InstutationHorizon() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
